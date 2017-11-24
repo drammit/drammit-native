05755f0e58a48f46237c861beb1d4f73a8acc9b9
Gaya Kessler <gaya.kessler@gmail.com>
2017-11-24 16:35:29 +0100

    add redux

--- app/App.js
+++ app/App.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import { NativeRouter, Route } from 'react-router-native';
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import Welcome from './Welcome';
@@ -11,11 +11,20 @@ import About from './About';
 
 import styles from './Styles';
 
+const store = createStore(
+  combineReducers({}),
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  ),
+);
+
 export default () => (
-  <NativeRouter>
-    <View style={styles.container}>
-      <Route exact path="/" component={Welcome} />
-      <Route path="/about" component={About} />
-    </View>
-  </NativeRouter>
+  <Provider store={store}>
+    <NativeRouter>
+      <View style={styles.container}>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/about" component={About} />
+      </View>
+    </NativeRouter>
+  </Provider>
 );
