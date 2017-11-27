3f53d08d5cec6e1023fbdf7788855e861145a328
Gaya Kessler <gaya.kessler@gmail.com>
2017-11-27 11:44:05 +0100

    add redux dev tools

--- app/App.js
+++ app/App.js
@@ -1,10 +1,11 @@
 // @flow
 
 import React from 'react';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
 import { NativeRouter, Route } from 'react-router-native';
 import { createStore, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
+import devTools from 'remote-redux-devtools';
 
 import Welcome from './Welcome';
 import About from './About';
@@ -12,11 +13,19 @@ import About from './About';
 import styles from './Styles';
 
 const store = createStore(
-  combineReducers({}),
+  combineReducers({
+    test: () => true,
+  }),
   compose(
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    devTools({
+      name: Platform.OS,
+      hostname: 'localhost',
+      port: 5678,
+    }),
   ),
 );
+
+console.log(window);
 
 export default () => (
   <Provider store={store}>
