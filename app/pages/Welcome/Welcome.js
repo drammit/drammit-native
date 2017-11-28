7ae2b67ae64f8ac8a3cf3f50f95a0e95dde07e29
Gaya Kessler <gaya.kessler@gmail.com>
2017-11-28 09:44:17 +0100

    welcome screen animations

--- app/pages/Welcome/Welcome.js
+++ app/pages/Welcome/Welcome.js
@@ -1,47 +1,180 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import type { Element } from 'react';
-import { Text, View, Button } from 'react-native';
-import { Link } from 'react-router-native';
+import { connect } from 'react-redux';
+import { Animated, Text, View, Button } from 'react-native';
 
 import ImageContainer from '../../components/Layout/ImageContainer';
 import Logo from '../../components/Logo/Logo';
 
 import { colors } from '../../Config.styles';
 
-import Schotland from './Scotland.jpg';
+import Scotland from './Scotland.jpg';
 
-import styles from './Welcome.styles';
+import { styles, style } from './Welcome.styles';
 
+type WelcomeType = {
+  continueWithFacebook: Function,
+  continueWithEmail: Function,
+  signUp: Function,
+};
 
-function Welcome(): Element<any> {
-  return (
-    <ImageContainer
-      source={Schotland}
-    >
-      <View style={styles.container}>
-        <Logo />
-        <View style={styles.intro}>
-          <Text style={styles.introDash}>——</Text>
-          <Text style={styles.introText}>Keep track of and rate the whiskies you drink</Text>
-          <Text style={styles.introDash}>——</Text>
-        </View>
+class Welcome extends Component<WelcomeType> {
+  state = {
+    fadeHeader: new Animated.Value(0),
+    fadeFacebook: new Animated.Value(0),
+    fadeEmail: new Animated.Value(0),
+    fadeSignup: new Animated.Value(0),
+  };
 
-        <View style={styles.buttons}>
-          <View style={styles.facebookButton}>
-            <Button color={colors.white} title="Continue with Facebook" />
+  componentDidMount() {
+    setTimeout(() => {
+      Animated.timing(
+        this.state.fadeHeader,
+        {
+          toValue: 1,
+          duration: 1500,
+        },
+      ).start();
+    }, 500);
+
+    setTimeout(() => {
+      Animated.timing(
+        this.state.fadeFacebook,
+        {
+          toValue: 1,
+          duration: 500,
+        },
+      ).start();
+    }, 2000);
+
+    setTimeout(() => {
+      Animated.timing(
+        this.state.fadeEmail,
+        {
+          toValue: 1,
+          duration: 500,
+        },
+      ).start();
+    }, 1750);
+
+    setTimeout(() => {
+      Animated.timing(
+        this.state.fadeSignup,
+        {
+          toValue: 1,
+          duration: 500,
+        },
+      ).start();
+    }, 1500);
+  }
+
+  render(): Element {
+    const { continueWithFacebook, continueWithEmail, signUp } = this.props;
+    const { fadeHeader, fadeFacebook, fadeEmail, fadeSignup } = this.state;
+
+    return (
+      <ImageContainer
+        source={Scotland}
+      >
+        <View style={styles.container}>
+          <Animated.View
+            style={{
+              ...style.header,
+              opacity: fadeHeader,
+              transform: [{
+                translateY: fadeHeader.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [20, 0],
+                }),
+              }],
+            }}
+          >
+            <Logo />
+            <View style={style.intro}>
+              <Text style={styles.introDash}>——</Text>
+              <Text style={styles.introText}>Keep track of and rate the whiskies you drink</Text>
+              <Text style={styles.introDash}>——</Text>
+            </View>
+          </Animated.View>
+
+          <View style={styles.buttons}>
+            <Animated.View
+              style={{
+                ...style.facebookButton,
+                opacity: fadeFacebook,
+                transform: [{
+                  translateY: fadeFacebook.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [-10, 0],
+                  }),
+                }],
+              }}
+            >
+              <Button
+                onPress={continueWithFacebook}
+                color={colors.white}
+                title="Continue with Facebook"
+              />
+            </Animated.View>
+            <Animated.View
+              style={{
+                ...style.emailButton,
+                opacity: fadeEmail,
+                transform: [{
+                  translateY: fadeEmail.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [-10, 0],
+                  }),
+                }],
+              }}
+            >
+              <Button
+                onPress={continueWithEmail}
+                color={colors.white}
+                title="Continue with email"
+              />
+            </Animated.View>
+            <Animated.View
+              style={{
+                ...style.signupButton,
+                opacity: fadeSignup,
+                transform: [{
+                  translateY: fadeSignup.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [-10, 0],
+                  }),
+                }],
+              }}
+            >
+              <Button
+                onPress={signUp}
+                color={colors.white}
+                title="New here? Sign up!"
+              />
+            </Animated.View>
           </View>
-          <View style={styles.emailButton}>
-            <Button color={colors.white} title="Continue with email" />
-          </View>
-          <Link to="/sign-up" style={styles.signupButton}>
-            <Button color={colors.white} title="New here? Sign up!" />
-          </Link>
         </View>
-      </View>
-    </ImageContainer>
-  );
+      </ImageContainer>
+    );
+  }
 }
 
-export default Welcome;
+function mapDispatchToProps(dispatch) {
+  return {
+    continueWithFacebook() {
+      console.log('Facebook');
+    },
+
+    continueWithEmail() {
+      console.log('Email');
+    },
+
+    signUp() {
+      console.log('Sign Up');
+    },
+  };
+}
+
+export default connect(null, mapDispatchToProps)(Welcome);
--- app/pages/Welcome/Welcome.styles.js
+++ app/pages/Welcome/Welcome.styles.js
@@ -1,17 +1,25 @@
 import { StyleSheet } from 'react-native';
 
 import { colors, sizes } from '../../Config.styles';
+import { darken } from '../../core/colors';
 
 const button = {
   marginTop: sizes.base,
+  borderBottomWidth: 3,
 };
 
-export default StyleSheet.create({
+export const style = {
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     width: '100%',
+  },
+  header: {
+    alignItems: 'center',
+    width: '100%',
+    position: 'absolute',
+    top: 180,
   },
   intro: {
     flexDirection: 'row',
@@ -36,8 +44,23 @@ export default StyleSheet.create({
   buttons: {
     marginTop: sizes.base,
     marginBottom: sizes.base,
+    width: '80%',
+    position: 'absolute',
+    bottom: 60,
   },
-  facebookButton: Object.assign({}, button, { backgroundColor: colors.facebook }),
-  emailButton: Object.assign({}, button, { backgroundColor: colors.lightGreen }),
-  signupButton: Object.assign({}, button, { borderColor: colors.white, borderWidth: 1 }),
-});
+  facebookButton: Object.assign({}, button, {
+    backgroundColor: colors.facebook,
+    borderBottomColor: darken(colors.facebook, 35),
+  }),
+  emailButton: Object.assign({}, button, {
+    backgroundColor: colors.lightGreen,
+    borderBottomColor: colors.darkGreen,
+  }),
+  signupButton: Object.assign({}, button, {
+    borderColor: colors.white,
+    borderWidth: 1,
+    borderBottomWidth: 1,
+  }),
+};
+
+export const styles = StyleSheet.create(style);
