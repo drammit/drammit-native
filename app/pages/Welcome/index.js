21b1a31da69439fb6c55e26ad63d753cac2e3fbd
Gaya Kessler <gaya.kessler@gmail.com>
2017-12-11 13:12:10 +0100

    remove unneeded routes

--- app/pages/Welcome/index.js
+++ app/pages/Welcome/index.js
@@ -15,11 +15,9 @@ function Routes({ match, location }: ReactRouterType): Element<any> {
     <AnimatedRoutes>
       <Switch location={location}>
         <Route exact path={`${match.path}`} component={Welcome} />
-        {/* drammit://reset-password?user=19&token=testing */}
+
         <Route path={`${match.path}forgot-password`} component={Forgot} />
         <Route path={`${match.path}reset-password`} component={Reset} />
-
-        {/*<Redirect from={`${match.path}`} to={`${match.path}sign-up/step-2`} />*/}
       </Switch>
     </AnimatedRoutes>
   );
